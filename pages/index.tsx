9116114c2b706fa6b1663fd86998adbcd412ba07
Rick Brown <ricky.brown.00@gmail.com>
2022-12-08 20:42:27 +0000

    adds cdn image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next'
-
+import Image from 'next/image'
 
 const Home: NextPage = () => {
-	return <main role='main'>APP</main>
+	return (
+		<main role='main'>
+			<Image
+				src={'https://d2d4rtarj3og8s.cloudfront.net/avatar.png'}
+				width={200}
+				height={200}
+				alt='avatar'
+			/>
+		</main>
+	)
 }
 
 export default Home
