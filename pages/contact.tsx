cab0bca16a6dea804ee4701d738e8173e1859361
Rick Brown <ricky.brown.00@gmail.com>
2022-12-13 13:41:55 +0000

    adds dummy contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+type Props = {}
+
+const Contact: FC<Props> = (): JSX.Element => {
+	return <h1>Contact</h1>
+}
+
+export default Contact
+
+// Path: pages/contact.tsx
+// Created at: 13:41:22 - 13/12/2022
+// Language: Typescript
+// Framework: React/Next.js
