73d1744ca8a1e5f9ea990abc675ce9a50080b51b
Rick Brown <ricky.brown.00@gmail.com>
2023-01-10 14:22:35 +0000

    adds layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,11 +8,9 @@ type Props = {
 
 const Layout: FC<Props> = ({ children }): JSX.Element => {
 	return (
-		<Flex h={'100vh'} w={'100vw'}>
-			<Container maxW={1080}>
-				<Navbar /> {/* This navbar is 80px tall */}
-				{children}
-			</Container>
+		<Flex h={'100vh'} w={'100vw'} flexDirection={'column'} paddingX={12}>
+			<Navbar /> {/* This navbar is 80px tall */}
+			<Container maxW={'container.md'}>{children}</Container>
 		</Flex>
 	)
 }
